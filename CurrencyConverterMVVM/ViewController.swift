cd06857d21833e395fbfca1e14a3a6a7fffb7189
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-04-07 14:12:59 -0300

    Initial Commit

---
+++ CurrencyConverterMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverterMVVM
+//
+//  Created by Lucas Fernandez Nicolau on 07/04/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
